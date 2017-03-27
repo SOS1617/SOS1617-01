837f8774a10b017436c9a13ec5383eb67632ad8b
SOS1617_brr <ruizbeatriz53@gmail.com>
2017-03-27 17:48:53 +0000

    heroku

--- f04.js
+++ f04.js
@@ -38,7 +38,7 @@ MongoClient.connect(mdbURL,{native_parser:true},function(err,database){
 
 
 var app = express();
-//app.use("/api/v1/",express.static(path.join('public')));
+app.use("/api/v1/",express.static(path.join('public')));
 app.use(bodyParser.json()); //use default json enconding/decoding
 app.use(helmet()); //improve security
 
