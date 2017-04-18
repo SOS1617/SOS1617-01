c21ad2f0d5e48ad7d5172ddf5a2a2a56ca5b77f3
SOS1617_brr <ruizbeatriz53@gmail.com>
2017-04-18 19:33:38 +0000

    paginacion

--- public/list-ctrl.js
+++ public/list-ctrl.js
@@ -3,11 +3,12 @@
   .controller("ListCtrl",["$scope","$http",function ($scope,$http){
                 console.log("Controller initialized");
             $scope.url="/api/v2/gvg";
+           
             function refresh(){}
             
             $scope.loadinitial=function(){
                 $http
-                .get($scope.url+"/loadInitialData?apikey=sos161701")
+                .get($scope.url+"/loadInitialData?apikey="+$scope.apikey)
                 .then(function(response){
                     console.log("LOADINITIAL 200 ok");
                 });
@@ -16,18 +17,32 @@
             $scope.listCountry= function(){
                     
                 $http
-                .get($scope.url+"?apikey=sos161701")
+                .get($scope.url+"?apikey="+$scope.apikey+"&limit="+$scope.limit+"&offset="+$scope.offset)
                 .then(function (response){
-                 
+                    $scope.data = JSON.stringify(response.data, null, 2);
                     $scope.gvg=response.data;
                     console.log("GET 200 ok");
                     
                     
                       });
             };
-            
+             $scope.paginacion= function(){
+                    
+                $http
+                .get($scope.url+"?apikey="+$scope.apikey+"&limit="+$scope.limit+"&offset="+$scope.offset)
+                .then(function (response){
+                 for(var i=$scope.limit;i<=$scope.offset;i++){
+                     $scope.gvg=response.data;
+                     $scope.gvg[i];
+                 }
+                    
+                    console.log("GET 200 ok");
+                    
+                    
+                      });
+            };
             $scope.editCountry=function(){
-                $http.put($scope.url+"/$scope.updateCountry.country?apikey=sos161701",$scope.updateCountry)
+                $http.put($scope.url+"/$scope.updateCountry.country?apikey="+$scope.apikey,$scope.updateCountry)
                 .then(function(){
                      console.log("PUT 200 ok");
                       refresh();
@@ -36,7 +51,7 @@
             };
                     
                $scope.addCountry= function(){
-                $http.post($scope.url+"?apikey=sos161701",$scope.newCountry)
+                $http.post($scope.url+"?apikey="+$scope.apikey,$scope.newCountry)
                 .then(function(response){
                   console.log("POST 200 ok");
                   refresh();
@@ -44,7 +59,7 @@
                     
                 }; 
                $scope.removeAll=function(){
-                   $http.delete($scope.url+"?apikey=sos161701")
+                   $http.delete($scope.url+"?apikey="+$scope.apikey)
                         .then(function(){
                             console.log("REMOVE 200 ok");
                             refresh();
@@ -53,7 +68,7 @@
                };
                   $scope.removeOne=function(country){
                   $http
-                .delete($scope.url+"/"+ country +"/?apikey=sos161701")
+                .delete($scope.url+"/"+ country +"/?apikey="+$scope.apikey)
                 .then(function(response){
                     console.log("DELETE one 200 ok");
                     refresh();
