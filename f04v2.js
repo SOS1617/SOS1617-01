29fb0595d0c404e23cdbe75e6ff8ae1ce6882b84
SOS1617_arg <alberto94_dh@hotmail.com>
2017-05-16 19:28:01 +0000

    subida alb

--- f04v2.js
+++ f04v2.js
@@ -122,3 +122,24 @@ app.get("/proxyirene", (req, res)=>{
               }
               http.request(options,callback).end();
  });
+ 
+ app.get("/proxyalb", (req,res) => {
+    var http=require('http');
+    var options={
+        host:'sos1617-03.herokuapp.com',
+        path:'/api/v2/earlyleavers/?apikey=apisupersecreta'
+    };
+    callback = function(response){
+       var str ='';
+       
+       response.on('data',function(chunk){
+          str+=chunk; 
+       });
+       response.on('end', function() {
+           res.send(str);
+       });
+       
+    }
+    
+    http.request(options, callback).end();
+});
