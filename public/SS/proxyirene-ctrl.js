05de0decd0028eadb9243724c7725cc024da4e39
SOS1617_ipf <irenepf93@gmail.com>
2017-05-16 20:27:52 +0000

    final final final

--- public/SS/proxyirene-ctrl.js
+++ public/SS/proxyirene-ctrl.js
@@ -39,7 +39,7 @@ angular
                     },
                     xAxis: {
                         categories: dato2.map(function(d) {
-                            return d.year;
+                            return d.country;
                         })
                     },
                     yAxis: {
